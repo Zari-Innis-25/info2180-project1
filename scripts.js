97f1abd0c5d5ad06d92c06d273043aad578ce457
Zari Innis <inniszari@gmail.com>
2025-11-23 18:43:28 -0500

    Added in Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded",function(){
+    let form= document.querySelector(".newsletter form");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        let message=document.querySelector(".newsletter .message");
+        let email= document.querySelector("#email").value;
+
+        if (email==""){
+            message.textContent= `Please enter a valid email address. `;
+
+        }
+        
+        else{
+        message.textContent= `Thank you! Your email address ${email} has been added to our mailing list! `;
+        }
+
+    });
+});
